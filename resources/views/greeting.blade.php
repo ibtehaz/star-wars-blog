08334c15601771f10b68c043d4c776c0f8430b93
ibtehaz <ibtehaz.utsay@gmail.com>
2022-04-03 00:31:51 -0700

    testing images

--- resources/views/greeting.blade.php
+++ resources/views/greeting.blade.php
@@ -1,6 +1,5 @@
-
-    <x-header />
-    <! -- Jumbotron -->
+<x-header />
+<! -- Jumbotron -->
     <div class="jumbotron">
         <div class="container">
             <h1 class="display-4">Hello, world!</h1>
@@ -10,5 +9,45 @@
             <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
         </div>
     </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <a href="/"><img src="https://www.fillmurray.com/150/255" alt=""></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="https://www.fillmurray.com/150/255" alt=""></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="https://www.fillmurray.com/150/255" alt=""></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="https://www.fillmurray.com/150/255" alt=""></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="https://www.fillmurray.com/150/255" alt=""></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="https://www.fillmurray.com/150/255" alt=""></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="https://www.fillmurray.com/150/255" alt=""></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="https://www.fillmurray.com/150/255" alt=""></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="https://www.fillmurray.com/150/255" alt=""></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="https://www.fillmurray.com/150/255" alt=""></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="https://www.fillmurray.com/150/255" alt=""></a>
+            </div>
+            <div class="col">
+                <a href="/"><img src="https://www.fillmurray.com/150/255" alt=""></a>
+            </div>
+        </div>
+    </div>
 
-<x-footer />
+    <x-footer />
