5ea2d8f1bb41a14ea9a8faab3e7798bcaf1a96c7
Angel Pena <penaangel1999@gmail.com>
2022-04-06 23:12:22 -0700

    header edits

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -20,8 +20,8 @@
                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                    <span class="navbar-toggler-icon"></span>
                </button>
-               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+               <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+                   <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                        <li class="nav-item">
                            <a class="nav-link active" aria-current="page" href="/">Home</a>
                        </li>
@@ -30,9 +30,9 @@
                        </li>
 
 
-                   </ul>
+                   </ul> -->
                   
-                   <form class="d-flex me-lg-3">
+                   <form class="d-flex me-lg-3" style= "margin-left: auto!important;">
                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                        <button class="btn btn-outline-danger" type="submit">Search</button>
                    </form>
