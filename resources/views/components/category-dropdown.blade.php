8e2307998b98ba42a8425494e259b984397b7042
ibtehaz <ibtehaz.utsay@gmail.com>
2022-04-14 13:53:10 -0700

    implement pagination with query params

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -8,14 +8,14 @@
 
         </button>
     </x-slot>
-<x-dropdown-item href="/" :active="request()->routeIs('home')">
+<x-dropdown-item href="/??{{ http_build_query(request()->except('category', 'page')) }}" :active="request()->routeIs('home')">
     Home
 </x-dropdown-item>
 
 
     @foreach($categories as $category)
 
-    <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}" :active='request()->is("categories/{$category->slug}")'>
+    <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}" :active='request()->is("categories/{$category->slug}")'>
     {{ ucwords($category->name)}}
 
     </x-dropdown-item>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,26 +4,10 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
         <x-posts-grid :posts="$posts" />
+        {{ $posts->links() }}
         @else
         <p class="text-center">No posts yet. Please check back later.</p>
         @endif
     </main>
-    <!-- @foreach ($posts as $post)
-    <article>
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {!! $post->title !!}
-            </a>
-        </h1>
-        <p>
-            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }} </a> in <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a>
 
-
-        </p>
-        <div>
-            {{ $post->excerpt }}
-        </div>
-
-    </article>
-    @endforeach -->
 </x-layout>
