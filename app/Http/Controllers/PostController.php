1880b727c6b493907f4e552fbbd58fe0fc2f966f
ibtehaz <ibtehaz.utsay@gmail.com>
2022-04-17 17:52:31 -0700

    limit access to only admin for creating posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class PostController extends Controller
 {
@@ -20,5 +21,27 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return view('posts.show', ['post' => $post]);
+    }
+
+    public function create() 
+    {
+       
+        return view('posts.create');
+    }
+
+    public function store() {
+        $attributes = request()->validate([
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('posts', 'slug')],
+            'excerpt' => 'required',
+            'body' => 'required',
+            'category_id' => ['required', Rule::exists('categories', 'id')]
+        ]);
+
+        $attributes['user_id'] = auth()->id();
+
+        Post::create($attributes);
+
+        return redirect('/');
     }
 }
