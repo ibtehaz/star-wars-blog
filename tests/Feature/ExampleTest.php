3f525b72642475d91c640dd61f007f3473743ab0
ibtehaz <ibtehaz.utsay@gmail.com>
2022-04-29 01:26:52 -0700

    testing out tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -18,4 +20,43 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testHomeRoute()
+   {
+       $this->get('/')->assertSee('Star Wars Blog')->assertDontSee('Harry Potter');
+      
+   }
+
+    public function testRegisterRoute()
+    {
+        $this->get('/register')->assertSee('Register')->assertDontSee('Logout');;
+    }
+    public function testLoginRoute()
+    {
+        $this->get('/login')->assertSee('Login')->assertDontSee('Voldemort');;
+    }
+    public function testNewPost() {
+        $post = new Post();
+        $post->title = 'Example Title';
+        $post->body = 'Example Body';
+        $post->user_id = 1;
+        $post->category_id = 1;
+        $post->slug = 'example-title';
+        $post->excerpt = 'Example Excerpt';
+        $this->assertEquals('Example Title', $post->title);
+    }
+    
+   public function test404()
+   {
+       $this->get('/404')->assertStatus(404);
+   }
+
+    public function testUser() {
+         $user = new User();
+         $user->name = 'Example User';
+         $this->assertEquals('Example User', $user->name);
+    }
+
+  
+
 }
